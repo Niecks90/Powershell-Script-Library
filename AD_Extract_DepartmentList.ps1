3a19ce5f8dffc05655589c58cb037326c4391ce2
Niecks90 <93446974+Niecks90@users.noreply.github.com>
2026-04-30 15:28:08 +0200

    Translate comments

--- AD_Extract_DepartmentList.ps1
+++ AD_Extract_DepartmentList.ps1
@@ -1,8 +1,7 @@
-
-# Charger le module Active Directory
+# Import module Active Directory
 Import-Module ActiveDirectory
 
-# Extraire tous les départements uniques des utilisateurs
+# Retrieve all unique departments for users
 Get-ADUser -Filter * -Properties Department |
     Where-Object { $_.Department -ne $null -and $_.Department -ne "" } |
     Select-Object -ExpandProperty Department |
